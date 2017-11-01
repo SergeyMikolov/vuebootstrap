f1e5a1df5b1fb340aa294180fd497a29215a22e9
sergey <sergeymikolov@gmail.com>
2017-11-01 11:39:26 +0200

    first comit navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="nav-bar">
+    <div :class="{'under-body':toggled}" @click="toggled = !toggled"></div>
+
+    <section class="bg-dark">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4" style="position: relative !important;">
+
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" @click="toggled = !toggled">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <a class="navbar-brand" href="#">Navbar</a>
+
+          <a href="#" class="navbar-toggle collapsed"></a>
+          <transition name="slide-fade">
+            <div class="collapse navbar-collapse" id="navbarCollapse" :class="{'side-bar':toggled, 'col-5':toggled}" v-if="toggled">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Disabled</a>
+                </li>
+              </ul>
+            </div>
+          </transition>
+        </nav>
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+    data:function(){
+      return {
+        toggled: false
+      }
+    }
+  }
+
+</script>
+
+<style>
+  @import "../assets/style/nav-bar.css";
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row text-center">
+    <div class="col-md-6 offset-md-3 col-sm-8 offset-sm-2 col-xs-10 offset-xs-1">
+      Notice that the key names within this object match the names that we specify within the ref attribute, and the values are the DOM elements. In this case, we can see that the key is myButton and that the value is a native button element which has nothing to do with Vue.js.
+
+      Therefore we can simply access the DOM element by accessing the name of our reference as a property on the $refs object. Let’s see that in action. As an example, I will change the text of the button when clicking it.
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.col {
+  border: 1px solid black;
+}
+</style>
